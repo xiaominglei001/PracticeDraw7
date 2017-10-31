448627aff586f7f6ffc39b018c63ae11b6efeeb0
mudoY <61540975@qq.com>
2017-10-31 18:40:13 +0800

    feat:finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice02/Practice02HsvEvaluatorLayout.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw7.practice.practice02;
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -48,10 +49,22 @@ public class Practice02HsvEvaluatorLayout extends RelativeLayout {
 
     private class HsvEvaluator implements TypeEvaluator<Integer> {
 
+        float[] startHsv = new float[3];
+        float[] endHsv = new float[3];
+        float[] currentHsv = new float[3];
+
         // 重写 evaluate() 方法，让颜色按照 HSV 来变化
         @Override
         public Integer evaluate(float fraction, Integer startValue, Integer endValue) {
-            return startValue;
+
+            Color.colorToHSV(startValue, startHsv);
+            Color.colorToHSV(endValue, endHsv);
+
+            currentHsv[0] = startHsv[0] + fraction * (endHsv[0] - startHsv[0]);
+            currentHsv[1] = startHsv[1] + fraction * (endHsv[1] - startHsv[1]);
+            currentHsv[2] = startHsv[2] + fraction * (endHsv[2] - startHsv[2]);
+
+            return Color.HSVToColor(currentHsv);
         }
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -49,10 +49,16 @@ public class Practice03OfObjectLayout extends RelativeLayout {
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
 
+        PointF currentPoint = new PointF();
+
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+
+            currentPoint.x = startValue.x + (endValue.x - startValue.x) * fraction;
+            currentPoint.y = startValue.y + (endValue.y - startValue.y) * fraction;
+
+            return currentPoint;
         }
     }
 }
